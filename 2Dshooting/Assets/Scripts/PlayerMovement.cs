609d55a664042811f5db4d74104d3a3422f90110
lucatrab1905 <72077556+lucatrab1905@users.noreply.github.com>
2024-10-20 21:42:51 +0900

    カメラと動き
    プレイヤーには一時的なアセットを使用しました。カメラがプレイヤーを追っているため、動いていないように見えますが、背景に何かを置くと動いているのがわかります。

---
+++ 2Dshooting/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    Rigidbody2D rb;
+    [HideInInspector]
+    public float lastHorizontalVector;
+    [HideInInspector]
+    public float lastVerticalVector;
+    [HideInInspector]
+    public Vector2 moveDir;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        InputManagement();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputManagement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY).normalized;
+
+        if(moveDir.x != 0)
+        {
+            lastHorizontalVector = moveDir.x;
+        }
+        if (moveDir.y != 0)
+        {
+            lastVerticalVector = moveDir.y;
+        }
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+    }
+}
